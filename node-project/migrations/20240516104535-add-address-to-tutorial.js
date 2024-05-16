8d2cd818f9d528fae2a74645f7ca7ce973b2e7c3
arvindegiz <arvind@egizsolution.comm>
2024-05-16 17:28:53 +0530

    first commit a

---
+++ node-project/migrations/20240516104535-add-address-to-tutorial.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('tutorials', 'address', {
+      type: Sequelize.STRING,
+      allowNull: true // Set allowNull as per your requirement
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('tutorials', 'address');
+  }
+};
